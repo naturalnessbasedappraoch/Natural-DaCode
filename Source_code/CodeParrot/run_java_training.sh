e1351ddc6a3c3b1d8301873fe542ec0dcd1d5064
naturalnessbasedappraoch <naturalnessbasedapproach@gmail.com>
2024-09-15 14:30:43 +0800

    Update run_java_training.sh

--- Source_code/CodeParrot/run_java_training.sh
+++ Source_code/CodeParrot/run_java_training.sh
@@ -7,4 +7,6 @@ python prepare_java_dataset.py
 python train_java_tokenizer.py
 
 # Train the model
-python train_java_model.py --output_dir codeparrot_java_model --num_train_epochs 10 --per_device_train_batch_size 4 --save_steps 1000 --eval_steps 1000 --logging_dir logs
+python train_java_model.py --output_dir codeparrot_java_model --num_train_epochs 10 --per_device_train_batch_size 8 --save_steps 500 --eval_steps 500 --logging_dir logs --learning_rate 5e-5 --gradient_accumulation_steps 4
+
+
